ab4ae1909828e66cbe8bd687d5476d0647100d4b
kWRizzz <krishnathey2j@gmail.com>
2026-03-19 22:16:56 +0530

    moving forward in auth setup

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,33 @@ app.use(cookiepareser())
 app.use('/api/user',userRouter)
 
 
+/*
+**@Route=Test apis
+**
+*/
+
+app.get('/test1',(req,res)=>{
+    res.send("Test1")
+})
+
+
+/*
+**@Route=Test apis
+**
+*/
+
+app.get('/test2',(req,res)=>{
+    res.send("Test2")
+})
+
 // app for production ready state
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(_dirname,"../frontend/dist")))
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(_dirname,"..frontend","dist","index.html"))
+    })
 }
 
 
